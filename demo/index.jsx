bc4035f6f8f8aefacc65e42ca843949ec3e440f7
alex <daixiaohao@alexdeMacBook-Pro.local>
2019-10-23 17:40:49 +0800

    feat: add demo

---
+++ demo/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+import './styles.css'
+import Scroller from '../libs/scroller'
+
+const App = () => {
+  const height = 400
+  const itemHeight = 60
+  const data = Array.from({ length: 1000 }).map((_, index) => index + 1)
+  const itemCreater = index => (
+    <p key={index} onClick={() => console.log(index)}>
+      {index}
+    </p>
+  )
+  return (
+    <div className="wrap">
+      <p>React Infinite Scroller Demo</p>
+      <Scroller
+        rootId={'scroller'}
+        height={height}
+        itemHeight={itemHeight}
+        data={data}
+        itemCreater={itemCreater}
+      />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ demo/webpack.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const rootPath = path.resolve(__dirname)
+
+const config = {
+  target: 'web',
+  mode: 'development',
+  entry: {
+    index: path.join(rootPath, 'index.jsx')
+  },
+  output: {
+    path: path.join(rootPath, 'js'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: require.resolve('babel-loader')
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: require.resolve('style-loader')
+          },
+          {
+            loader: require.resolve('css-loader')
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['index'],
+      inject: true,
+      template: path.join(rootPath, 'index.html'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+    })
+  ],
+  devServer: {
+    port: 8000,
+    compress: true,
+    historyApiFallback: true,
+    hot: true
+  }
+}
+
+module.exports = config
